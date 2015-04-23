ce2bccf5981b2d20bdb77f7336be8c107ea32329
Anthony Parks <acousticscommander@gmail.com>
2015-04-23 12:58:06 -0400

    Update cookEQ.m
    fixed ':' errors

--- cookEQ.m
+++ cookEQ.m
@@ -17,56 +17,56 @@ alpha = sin(w0)/(2*Q);
 
 
 switch filterType
-  case 'lp':
+  case 'lp'
     b0 =  (1 - cos(w0))/2;
     b1 =   1 - cos(w0);
     b2 =  (1 - cos(w0))/2;
     a0 =   1 + alpha;
     a1 =  -2*cos(w0);
     a2 =   1 - alpha;
-  case 'hp':
+  case 'hp'
     b0 =  (1 + cos(w0))/2;
     b1 = -(1 + cos(w0));
     b2 =  (1 + cos(w0))/2;
     a0 =   1 + alpha;
     a1 =  -2*cos(w0);
     a2 =   1 - alpha;
-  case 'bp':
+  case 'bp'
     b0 =   alpha;
     b1 =   0;
     b2 =  -alpha;
     a0 =   1 + alpha;
     a1 =  -2*cos(w0);
     a2 =   1 - alpha;
-  case 'notch':
+  case 'notch'
     b0 =   1;
     b1 =  -2*cos(w0);
     b2 =   1;
     a0 =   1 + alpha;
     a1 =  -2*cos(w0);
     a2 =   1 - alpha;
-  case 'ap':
+  case 'ap'
     b0 =   1 - alpha;
     b1 =  -2*cos(w0);
     b2 =   1 + alpha;
     a0 =   1 + alpha;
     a1 =  -2*cos(w0);
     a2 =   1 - alpha;
-  case 'peak':
+  case 'peak'
     b0 =   1 + alpha*A;
     b1 =  -2*cos(w0);
     b2 =   1 - alpha*A;
     a0 =   1 + alpha/A;
     a1 =  -2*cos(w0);
     a2 =   1 - alpha/A;
-  case 'lowShelf':
+  case 'lowShelf'
     b0 =    A*( (A+1) - (A-1)*cos(w0) + 2*sqrt(A)*alpha );
     b1 =  2*A*( (A-1) - (A+1)*cos(w0)                   );
     b2 =    A*( (A+1) - (A-1)*cos(w0) - 2*sqrt(A)*alpha );
     a0 =        (A+1) + (A-1)*cos(w0) + 2*sqrt(A)*alpha;
     a1 =   -2*( (A-1) + (A+1)*cos(w0)                   );
     a2 =        (A+1) + (A-1)*cos(w0) - 2*sqrt(A)*alpha;
-  case 'highShelf':
+  case 'highShelf'
     b0 =    A*( (A+1) + (A-1)*cos(w0) + 2*sqrt(A)*alpha );
     b1 = -2*A*( (A-1) + (A+1)*cos(w0)                   );
     b2 =    A*( (A+1) + (A-1)*cos(w0) - 2*sqrt(A)*alpha );
@@ -78,5 +78,5 @@ end
 c0 = b0/a0;
 c1 = b1/a0;
 c2 = b2/a0;
-c2 = a1/a0;
+c3 = a1/a0;
 c4 = a2/a0;
